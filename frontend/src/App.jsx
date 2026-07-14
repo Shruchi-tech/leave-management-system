a5f7fb5e4ec438c404cbfbf7a963fef717d34e61
Shruchi-tech <shruchijha1234@gmail.com>
2026-07-14 12:36:25 +0530

    Initial project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Leave Management system</h1>
+        <h6>Welcoe to leave Manangement system made by shruchi.</h6>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
