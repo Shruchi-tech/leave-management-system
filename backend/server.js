c3182455c9a6d26c14f69a1337b10f5e5c57dfe1
Shruchi-tech <shruchijha1234@gmail.com>
2026-07-14 19:26:41 +0530

    database connected to the server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,23 @@
-
+require("dotenv").config();
+const { assert } = require("node:console");
 const app= require("./src/app.js");
+const pool=require("./src/db/db.js");
+const port= process.env.PORT||5000;
 
-const port= 5000;
-app.listen(port,()=>{
-    console.log(`Server is running at ${port}`)
-})
+async function startServer() {
+    try{
+        const connection=await pool.getConnection();
+        console.log("Databse connected");
+        connection.release();
+
+        app.listen(port,()=>{
+         console.log(`Server is running at ${port}`)
+        });
+
+    }
+    catch(err){
+        console.log("database connection failed");
+        console.error(err.message);
+    }
+}
+startServer();
--- backend/src/app.js
+++ backend/src/app.js
@@ -4,5 +4,7 @@ const cors= require("cors");
  const app=express();
  app.use(express.json());
  app.use(cors());
-
+ app.get("/",(req,res)=>{
+    res.send("Leave Management API");
+ });
  module.exports =app;
